17b2874958285d62b2c4782e1327c4b0f6701ff5
Zaini786a <144918061+Zaini786a@users.noreply.github.com>
2024-02-22 12:29:22 +0500

    special type

---
+++ specialtype .ts
@@ -0,0 +1,17 @@
+ //let nam = true
+ //nam:"string"
+ //Math.round(nam)  error not assign any type
+
+ let s:string = "Pakistan";
+ let n:number = 12345;
+ let bol:boolean = true;
+
+console.log(s);
+
+console.log(n);
+
+console.log(bol);
+
+
+
+ 
