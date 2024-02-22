5441b503f04b9b74f96b99b00461239aa3e225b8
Zaini786a <144918061+Zaini786a@users.noreply.github.com>
2024-02-22 16:47:42 +0500

    casting type

---
+++ casting.ts
@@ -0,0 +1,10 @@
+//cast with as 
+const nam = 'Hafiz Muhammad Ahmed'
+console.log((nam as string).length);
+
+//cast with <>
+
+const nam2 = 'Muhammad Tayyab'
+console.log((<string>nam2).length);
+
+
