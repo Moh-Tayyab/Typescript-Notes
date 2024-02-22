89a599dd28f025ee16de45bef3c8935a1fbb5c13
Zaini786a <144918061+Zaini786a@users.noreply.github.com>
2024-02-22 13:38:57 +0500

    object type

---
+++ typeObject.ts
@@ -0,0 +1,19 @@
+const person:{name:string , fname:string , age:number} = 
+            {
+                name : 'Usman',
+                
+                fname : 'Bilal',
+                age : 23   
+            };
+const person1:{name:string,fname:string ,age:number} = 
+           {
+            name : "Fahad",
+            fname : "Qasim",
+            age : 21
+           }
+
+                console.log(person);
+                console.log(person1);
+                
+                
+                
