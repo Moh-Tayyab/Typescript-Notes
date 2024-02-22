044b67f4aa1a1b6423e9496e369ce48a2400034b
Zaini786a <144918061+Zaini786a@users.noreply.github.com>
2024-02-22 13:52:14 +0500

    union type

---
+++ uniontype.ts
@@ -0,0 +1,17 @@
+/*
+function utype(entryPass:string|number|boolean){
+    console.log(`The student is ${entryPass} and gained ${entryPass} in the test.The entry pass is ${entryPass}`);
+
+    
+}
+
+utype("Pass");
+utype(92);
+utype(true);*/
+//function printed 3 times
+
+const var2:string | boolean | number = "Pakistan" //we can also added number=12345 and boolean= true/false 
+
+
+    console.log(var2);
+    
