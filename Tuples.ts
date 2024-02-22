590ab981fd87c346f2a84583a7e5dbd0c93db5f1
Zaini786a <144918061+Zaini786a@users.noreply.github.com>
2024-02-22 12:48:26 +0500

    tuple

---
+++ Tuples.ts
@@ -0,0 +1,11 @@
+let tupleExample :[string , boolean , number];
+
+tupleExample = ["Hello" , true , 22];
+
+tupleExample.unshift(false);
+
+tupleExample.push(44, "Dear student your future is more important");
+
+//tupleExample.pop()
+
+console.log(tupleExample)
